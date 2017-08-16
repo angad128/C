67f06b2b2406550e0dd2c785b4fe7cc08df4b3f5
Angad Budhathoki <30298033+angad129@users.noreply.github.com>
2017-08-17 01:28:23 +0545

    string is array in C
    1. strcpy(); is built in function in C that takes two parameters, variable and value to copy in it.

--- main.c
+++ main.c
@@ -3,15 +3,22 @@
 
 int main()
 {
-   // defining/declaring variable
-    int c;
-    int b;
-    int a;
+   //defining array with length of string = n+1; where n is actual length of string including space in between.
+    char nam[17] = "Angad Budhathoki";
+    printf("My name is %s\n",nam);
+   
+    nam[0] = 'K';
+    nam[1] = 'e';
+    nam[2] = 'R';
+    nam[3] = 'p';
+    nam[4] = 'T';
+    printf("My name is %s\n",nam);
 
-    c = 2017;
-    b = 1993;
-    a = c - b;
+    char food[] = "Choila";
+    printf("My fav food is %s.\n",food);
 
-    printf("My current age is %d ",a);
+    strcpy(food,"MoMos");
+    printf("My fav food is %s.\n",food);
+   
     return 0;
 }
