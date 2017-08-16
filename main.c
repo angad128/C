f8cdd45a5550f3f49d363c0266fe605b28a99a55
Angad Budhathoki <30298033+angad129@users.noreply.github.com>
2017-08-17 01:06:20 +0545

    use of conversion characters
    %s for strings
    $d for  Integers
    %f for floating numbers

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    printf("Good Morning!\n");
+    printf("%s are the best %s ever.\n","You","Hero");
+    printf("I ate %d momos last nigth\n", 9);
+    printf("Value of Pi is roughly %f.",3.1415926535);
+    printf("Value of Pi is roughly %.2f.",3.1415926535);
+    printf("Value of Pi is roughly %.4f.",3.1415926535);
     return 0;
 }
